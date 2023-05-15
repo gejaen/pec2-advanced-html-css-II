4590ff4895b312d436416fb3acbd6a4914ff10ff
gejaen <114834576+gejaen@users.noreply.github.com>
2023-05-15 13:57:22 +0200

    Add files via upload

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,24 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+
+// import 'some-node-module';
+// import SomeModule from 'some-node-module';
+
+/**
+ * Write any other JavaScript below
+ */
+
++( function() {
+  const university = "UOC";
+  console.log(`Hello, ${university}!`);
+} )();
+
+import { Card as Card, Carousel as Carousel, Modal as Modal, Button as Button } from 'bootstrap';
+
+
+
+
+
+
